4e13ebb40ba2125211132b9578866467488aa87f
David Newman <davidjndev@gmail.com>
2021-11-04 09:42:27 -0500

    getComments function retrieves all top level comments from a post

--- hacker-news-jobs/src/chrome/content.ts
+++ hacker-news-jobs/src/chrome/content.ts
@@ -1,5 +1,31 @@
 import { ChromeMessage, Sender } from "../types";
 import HackerNews from './hn-api';
+import HackerNewsItem from './hn-api';
+
+async function getKidIdsFromStory(storyId: number): Promise<number[] | null> {
+    try {
+        return await HackerNews.getItem(storyId)
+            .then((item: any) => {
+                return item['kids']
+            });
+    } catch {
+        return null
+    }
+}
+
+async function getComments(storyId: number, truncate: boolean) {
+    return getKidIdsFromStory(storyId)
+        .then(async (kidIds: number[] | null) => {
+            if (!kidIds) {
+                return null
+            }
+            if (truncate) {
+                kidIds = kidIds.slice(0, 10);
+            }
+            const all = kidIds.map(id => HackerNews.getItem(id))
+            return await Promise.all(all);
+        });
+}
 
 const messagesFromReactAppListener = (
     message: ChromeMessage,
@@ -23,11 +49,20 @@ const messagesFromReactAppListener = (
         sender.id === chrome.runtime.id &&
         message.from === Sender.React &&
         message.message === 'APITest') {
-        HackerNews.getStories(HackerNews.TYPE_TOP, 0, 5)
-            .then((stories:any) => {
-                let i = 1;
-                stories.forEach((story:any) => console.log(`${i++}. ${story.title} [${story.score}] (${story.url})}`))
-            });
+
+        // HackerNews.getStories(HackerNews.TYPE_TOP, 0, 5)
+        //     .then((stories: any) => {
+        //         let i = 1;
+        //         stories.forEach((story: any) => console.log(`${i++}. ${story.title} [${story.score}] (${story.url})}`))
+        //     });
+
+        const nov2021WhoIsHiringId = 29067493
+        // TRUNCATE kidIds for testing
+        const truncate = true
+        getComments(nov2021WhoIsHiringId, truncate)
+            .then((jobPostings) => {
+                console.log(jobPostings)
+            })
     }
 }
 
