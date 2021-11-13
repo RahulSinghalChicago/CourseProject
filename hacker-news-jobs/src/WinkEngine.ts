c318efa556db06e33d2aa2875d5672c1e9599e28
David Newman <davidjndev@gmail.com>
2021-11-13 09:47:23 -0600

    wink test working

--- hacker-news-jobs/src/WinkEngine.ts
+++ hacker-news-jobs/src/WinkEngine.ts
@@ -1,19 +1,27 @@
 import winkNLP from 'wink-nlp';
-// const model = require('wink-eng-lite-web-model');
-// const nlp = winkNLP(model);
+const model = require('wink-eng-lite-web-model');
+const BM25Vectorizer = require('wink-nlp/utilities/bm25-vectorizer');
 
 export default function runWinkTest() {
     console.log('in runWinkTest');
-    // Acquire "its" and "as" helpers from nlp.
-    // const its = nlp.its;
+
+    const nlp = winkNLP(model);
+    const bm25 = BM25Vectorizer();
+
+    const its = nlp.its;
     // const as = nlp.as;
 
-    // const text = `Its quarterly profits jumped 76% to $1.13 billion for the three months to December, from $639million of previous year.`;
-    // const doc = nlp.readDoc(text);
+    // Sample corpus.
+    const corpus = ['Bach', 'J Bach', 'Johann S Bach', 'Johann Sebastian Bach'];
+    // Train the vectorizer on each document, using its tokens. The tokens are
+    // extracted using the .out() api of wink NLP.
+    corpus.forEach((doc) => bm25.learn(nlp.readDoc(doc).tokens().out(its.normal)));
 
-    // doc.entities().each((e) => e.markup());
-    // document.getElementById("result").innerHTML = doc.out(its.markedUpText);
-    // console.log(ans)
+    // Returns the vector of the new document, "Johann Bach symphony", which is
+    // first tokenized using winkNLP.
+    const ans = bm25.vectorOf(nlp.readDoc('Johann Bach symphony').tokens().out(its.normal));
+    // -> [0.092717254, 0, 0.609969519, 0, 0]
+    console.log(ans)
 }
 
 
